d1b61573f136198c3a9b6d964b745bc69d53747f
changwon <ckddnjs554@naver.com>
2020-10-29 23:12:07 +0900

    LoserPlayer Array

--- src/arraydivision/Test.java
+++
@@ -1,9 +0,0 @@
-package arraydivision;
-
-public class Test {
-    public static void main(String[] args) {
-
-        System.out.println("Test 입니다");
-        System.out.println("안녕");
-    }
-}
---
+++ src/loserPlayer/LoserPlayer.java
@@ -0,0 +1,29 @@
+package loserPlayer;
+
+import java.util.Arrays;
+
+public class LoserPlayer {
+    public static void main(String[] args) {
+
+        LoserPlayer loserPlayer = new LoserPlayer();
+
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
+
+        loserPlayer.solution(participant, completion);
+    }
+
+    public String solution(String[] participant, String[] completion) {
+
+        String answer = "";
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i = 0;
+        for(i = 0; i < completion.length; i++){
+            if(!participant[i].equals(completion[i])){
+                return participant[i];
+            }
+        }
+        return participant[i];
+    }
+}
