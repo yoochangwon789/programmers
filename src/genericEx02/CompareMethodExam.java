42326efd5a021bb62c2703e671b7de945c379393
changwon <ckddnjs554@naver.com>
2020-11-05 19:40:30 +0900

    Generic_Method02

---
+++ src/genericEx02/CompareMethodExam.java
@@ -0,0 +1,28 @@
+package genericEx02;
+
+public class CompareMethodExam {
+    public static void main(String[] args) {
+
+        Pair<Integer, String> p1 = new Pair<>(1, "사과");
+        Pair<Integer, String> p2 = new Pair<>(1, "사과");
+
+        boolean result = Util.compare(p1, p2);
+
+        if(result) {
+            System.out.println("논리적으로 동등한 객체입니다.");
+        } else {
+            System.out.println("논리적으로 동등하지 않은 객체입니다.");
+        }
+
+        Pair<String, String> p3 = new Pair<>("user1", " Java");
+        Pair<String, String> p4 = new Pair<>("user2", " Java");
+
+        boolean result2 = Util.compare(p3, p4);
+
+        if(result2) {
+            System.out.println("논리적으로 동등한 객체 입니다.");
+        } else {
+            System.out.println("논리적으로 동등하지 않은 객체입니다.");
+        }
+    }
+}
