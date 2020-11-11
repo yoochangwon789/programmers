c375c103a83053875aa0234360499fc33c40a303
changwon <ckddnjs554@naver.com>
2020-11-11 21:32:29 +0900

    서로다른 인덱스 더하기

--- src/selecttwosum/SelectTwoSum.java
+++ src/selecttwosum/SelectTwoSum.java
@@ -10,26 +10,34 @@ public class SelectTwoSum {
 
         int[] numbers = {2,1,3,4,1};
 
-        System.out.println(solution(numbers));
+        SelectTwoSum selectTwoSum = new SelectTwoSum();
+
+        int[] answer = selectTwoSum.solution(numbers);
+
+        for(int i = 0; i < answer.length; i++) {
+            System.out.print(answer[i]);
+        }
     }
 
-    static public int[] solution(int[] numbers) {
+    public int[] solution(int[] numbers) {
 
         List<Integer> list = new ArrayList<>();
-        int[] answer;
+        int answer[];
 
-        for(int i = 0; i < numbers.length; i++) {
-            for(int j = i + 1; j <= numbers.length; j++) {
+        for(int i = 0; i < numbers.length - 1; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
 
                 int c = numbers[i] + numbers[j];
-                list.add(c);
+
+                if(list.indexOf(c) < 0) {
+                    list.add(c);
+                }
             }
         }
 
         answer = new int[list.size()];
 
         for(int i = 0; i < answer.length; i++) {
-
             answer[i] = list.get(i);
         }
 
