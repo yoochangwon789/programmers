10a65743d4d0cb563f14d480a396509ce89a32b1
changwon <ckddnjs554@naver.com>
2020-11-23 00:30:08 +0900

    Stack_Queue_Clear

--- src/pinterStackQueue/PrinterStackQueue.java
+++ src/pinterStackQueue/PrinterStackQueue.java
@@ -6,7 +6,6 @@ public class PrinterStackQueue {
     public static void main(String[] args) {
 
         ArrayList<Integer> prioritiesList = new ArrayList<>();
-        ArrayList<Integer> answerList = new ArrayList<>();
 
         prioritiesList.add(2);
         prioritiesList.add(1);
@@ -14,11 +13,15 @@ public class PrinterStackQueue {
         prioritiesList.add(2);
 
         for(int i = 0; i < prioritiesList.size(); i++) {
-            if(prioritiesList.get(0) < prioritiesList.get(i + 1)) {
-                prioritiesList.remove(prioritiesList.get(0));
+            for(int j = 1; j < prioritiesList.size(); j++) {
+
+                if(prioritiesList.get(0) < prioritiesList.get(j)) {
+                    prioritiesList.add(prioritiesList.get(0));
+                    prioritiesList.remove(0);
+                }
             }
         }
 
-        System.out.println(answerList);
+        System.out.println(prioritiesList.size());
     }
 }
