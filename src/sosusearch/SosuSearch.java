ed56019281d254cd45cd3a519d32d1a23b72eb6d
changwon <ckddnjs554@naver.com>
2020-11-24 22:30:26 +0900

    Sosu

---
+++ src/sosusearch/SosuSearch.java
@@ -0,0 +1,15 @@
+package sosusearch;
+
+public class SosuSearch {
+    public static void main(String[] args) {
+
+        int a = 2;
+
+        if(a % 2 == 0) {
+            System.out.println("짝수입니다");
+        }
+        else {
+            System.out.println("홀수 입니다");
+        }
+    }
+}
