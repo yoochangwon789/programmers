4cb86106de640910cdf4fbd1f7d5dae946f3a268
changwon <ckddnjs554@naver.com>
2021-01-02 01:29:04 +0900

    프린터 : 프로그래머스 Stack & Queue 알고리즘

--- src/pinterStackQueue/PrinterStackQueue.java
+++ src/pinterStackQueue/PrinterStackQueue.java
@@ -5,22 +5,35 @@ import java.util.ArrayList;
 public class PrinterStackQueue {
     public static void main(String[] args) {
 
-        int[] priorities = {1,1,9,1,1,1};
+        int[] priorities = {1, 1, 9, 1, 1, 1};
         PrinterStackQueue printerStackQueue = new PrinterStackQueue();
 
-        System.out.println(printerStackQueue.solution(priorities, 0));
+        System.out.println(printerStackQueue.solution(priorities, 2));
     }
 
     public int solution(int[] priorities, int location) {
         int answer = 0;
         int cnt = 0;
 
-        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> arrayList1 = new ArrayList<>();
+        ArrayList<Integer> locList = new ArrayList<>();
 
-        for (int i = 0; i < priorities.length; i++) arrayList.add(priorities[i]);
+        for (int i = 0; i < priorities.length; i++) {
+            arrayList1.add(priorities[i]);
+            locList.add(i);
+        }
 
-        while (!arrayList.isEmpty()) {
+        while (true) {
+            int i = arrayList1.get(0);
 
+            arrayList1.remove(i);
+
+            if (arrayList1.get(location) > i) {
+                arrayList1.add(i);
+            } else {
+                answer += cnt + 1;
+                break;
+            }
         }
 
         return answer;
