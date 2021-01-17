1d78fa52b4e35fd4000f46be6f9d3346503e7079
changwon <ckddnjs554@naver.com>
2021-01-18 01:35:19 +0900

    Feat : 두 개 뽑아서 더하기 해결!!

--- src/twonumberssum/TwoNumbersSum.java
+++ src/twonumberssum/TwoNumbersSum.java
@@ -1,4 +1,43 @@
 package twonumberssum;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class TwoNumbersSum {
+    public static void main(String[] args) {
+
+        int[] numbers = {2,1,3,4,1};
+
+        for (int i : solution(numbers)) {
+            System.out.println(i);
+        }
+    }
+
+    public static int[] solution(int[] numbers) {
+        int[] answer = {};
+        ArrayList<Integer> arrayList1 = new ArrayList<>();
+        ArrayList<Integer> arrayList2 = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                arrayList1.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        for (int i : arrayList1) {
+            if (!arrayList2.contains(i)) {
+                arrayList2.add(i);
+            }
+        }
+
+        Collections.sort(arrayList2);
+
+        answer = new int[arrayList2.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = arrayList2.get(i);
+        }
+
+        return answer;
+   }
 }
--- src/twonumberssum/TwoNumbersSumTest.java
+++ src/twonumberssum/TwoNumbersSumTest.java
@@ -1,18 +1,33 @@
 package twonumberssum;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 public class TwoNumbersSumTest {
     public static void main(String[] args) {
 
-        int[] numbers = {2,1,3,4,1};
-
-        Set<Integer> hashSet = new HashSet<>();
-
-        for (int i = 1; i < numbers.length; i++) {
-            System.out.println(numbers[i - 1] + numbers[i]);
-        }
+//        int[] numbers = {5,0,2,7};
+//
+//        ArrayList<Integer> arrayList1 = new ArrayList<>();
+//        ArrayList<Integer> arrayList2 = new ArrayList<>();
+//        int[] answer = {};
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            for (int j = i + 1; j < numbers.length; j++) {
+//                arrayList1.add(numbers[i] + numbers[j]);
+//            }
+//        }
+//
+//        for (int i : arrayList1) {
+//            if (!arrayList2.contains(i)) {
+//                arrayList2.add(i);
+//            }
+//        }
+//
+//        Collections.sort(arrayList2);
+//
+//        System.out.println(arrayList2);
     }
 }
